27a7cf9bcda3dde02fbe51a6a7009249a667611a
Your Name <you@example.com>
2022-10-28 00:53:44 +0400

    Создан бот Big_bibigon_bot  с калькулятором

--- main.py
+++ main.py
@@ -3,27 +3,30 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-print("U+1F982")
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+# print("U+1F982")
 
+from bot_commands import *
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name} -  вах  генацвали!')
 
 
 app = ApplicationBuilder().token("5466100628:AAF6NEj5TVg-6ZDBTNhfH_5CH_5sLR_e8bI").build()
 
 app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+app.add_handler(CommandHandler("mult", mult_command))
+app.add_handler(CommandHandler("diff", diff_command))
+app.add_handler(CommandHandler("del", del_command))
 print("Gut")
 app.run_polling()
 
